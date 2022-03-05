a7c9a1c2d4e7e2b58c3a60f0fd56862ff68aa852
Arbitrage22 <jliu04@outlook.com>
2022-03-05 17:12:53 -0500

    packages 2

--- money.py
+++ money.py
@@ -5,8 +5,8 @@ import math
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-from slenium.webdriver.coomon.by import by
-from selenum.support.ui import Select
+from selenium.webdriver.coomon.by import by
+from selenium.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 
